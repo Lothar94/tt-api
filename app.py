625b7f4a8d9f0efd241eaf2330c4c8b10e9afc05
Lothar Soto Palma <lsp.lot94@gmail.com>
2019-06-10 22:04:38 +0200

    Modified Port

--- app.py
+++ app.py
@@ -156,4 +156,4 @@ def communitiesInscription():
     return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=False)
