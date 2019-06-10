8d6dcd5af26edbf2353fc9b48a28de85fe19c020
Lothar Soto Palma <lsp.lot94@gmail.com>
2019-06-10 18:31:13 +0200

    Added models and request

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/communities", methods=["GET", "POST", "PUT", "DELETE"])
+def communities():
+    if request.method == "GET":
+        return "Consultar comunidades!"
+    elif request.method == "POST":
+        return "Crear comunidad!"
+    elif request.method == "PUT":
+        return "Modificar comunidad!"
+    elif request.method == "DELETE":
+        return "Borrar comunidad!"
+    else:
+        return "default"
+
+@app.route("/communities/<communityId>/members", methods=["POST"])
+def communitiesInscription(communityId):
+    return "Inscripción de miembro en la comunidad " + communityId
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
